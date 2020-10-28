b9d205ffae54fc3e9548c7a6a09d80bf79076fb6
BrandinGoldsberry <bgoldsberry@student.neumont.edu>
2020-10-28 11:17:59 -0600

    Users can now upload images

--- image-edit.js
+++ image-edit.js
@@ -1,1 +1,17 @@
-. 
+var imageEdit = {
+    currentImage: {}
+}
+
+window.onload = (ev) => {
+    document.getElementById("upload-image").addEventListener("change", (imageEvent) => {
+        imageEdit.currentImage = imageEvent.target.files[0];
+        if (FileReader) {
+            var fr = new FileReader();
+            fr.onload = function () {
+                document.getElementById("preview").src = fr.result;
+            }
+            fr.readAsDataURL(imageEvent.target.files[0]);
+        }
+    
+    })
+};
