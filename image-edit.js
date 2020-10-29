c53a8d27169b8bc9aad9507e8270165986c27539
shutdoor <enyssa2129@gmail.com>
2020-10-29 10:28:46 -0600

    Grade.JS
    Added Grade.JS library & functionality, and necessary styling to make it look nice and function.

--- image-edit.js
+++ image-edit.js
@@ -1,1 +1,11 @@
-. 
+window.addEventListener("load", function(){
+    Grade(document.querySelectorAll("#gradientWrap"));
+
+    var img = document.querySelector("#image");
+    img.naturalWidth
+
+    // document.getElementById("gradientWrap").style.width = `${img.naturalWidth+40}px`;
+    // console.log(img.naturalWidth);
+    document.getElementById("gradientWrap").style.height = `${img.naturalHeight+60}px`;
+    console.log(img.naturalHeight);
+})
