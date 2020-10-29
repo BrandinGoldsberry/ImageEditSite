940ad65a77efd2dd77be78f4366313d32b6cf0b3
Xlayton <weightequalsmasstimesgravity@gmail.com>
2020-10-28 20:55:44 -0600

    Caman basic functionality

--- image-edit.js
+++ image-edit.js
@@ -2,6 +2,11 @@ var imageEdit = {
     currentImage: {}
 }
 
+let brightness = 10;
+let contrast = 10;
+let sepia = 10;
+let saturation = 10;
+
 window.onload = (ev) => {
     document.getElementById("upload-image").addEventListener("change", (imageEvent) => {
         imageEdit.currentImage = imageEvent.target.files[0];
@@ -9,9 +14,16 @@ window.onload = (ev) => {
             var fr = new FileReader();
             fr.onload = function () {
                 document.getElementById("preview").src = fr.result;
+                Caman("#preview", function () {
+                    this.brightness(brightness)
+                    this.contrast(contrast)
+                    this.sepia(sepia)
+                    this.saturation(saturation)
+                    this.render()
+                })
             }
             fr.readAsDataURL(imageEvent.target.files[0]);
         }
-    
+
     })
 };
