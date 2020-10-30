4389bfc77fdd4e011cd293b0ea407150e4977b8f
Xlayton <weightequalsmasstimesgravity@gmail.com>
2020-10-29 21:57:51 -0600

    Compressor sliders to change values

--- image-edit.js
+++ image-edit.js
@@ -2,6 +2,11 @@ var imageEdit = {
     currentImage: {}
 }
 
+const compressorHeightSlider = document.getElementById("compressorHeight")
+const compressorWidthSlider = document.getElementById("compressorWidth")
+const compressorHeightLabel = document.getElementById("compressorHeightLabel")
+const compressorWidthLabel = document.getElementById("compressorWidthLabel")
+
 let height = 50
 let width = 50
 
@@ -14,6 +19,12 @@ window.onload = (ev) => {
             var fr = new FileReader();
             fr.onload = function () {
                 document.getElementById("preview").src = fr.result;
+                compressorHeightSlider.max = document.getElementById("preview").height
+                compressorHeightSlider.value = document.getElementById("preview").height
+                compressorHeightLabel.innerText = `Height: ${document.getElementById("preview").height}`
+                compressorWidthSlider.max = document.getElementById("preview").width
+                compressorWidthSlider.value = document.getElementById("preview").width
+                compressorWidthLabel.innerText = `Height: ${document.getElementById("preview").width}`
             }
             fr.readAsDataURL(imageEvent.target.files[0]);
         }
@@ -27,8 +38,26 @@ function compressor() {
         success(result) {
             imageEdit.currentImage = result
             document.getElementById("preview").src = URL.createObjectURL(result);
+            compressorHeightSlider.max = document.getElementById("preview").height
+            compressorHeightSlider.value = document.getElementById("preview").height
+            compressorHeightLabel.innerText = `Height: ${document.getElementById("preview").height}`
+            compressorWidthSlider.max = document.getElementById("preview").width
+            compressorWidthSlider.value = document.getElementById("preview").width
+            compressorWidthLabel.innerText = `Width: ${document.getElementById("preview").width}`
         }
     })
 }
 
+function changeCompressorHeight(evt) {
+    height = evt.target.value
+    compressorHeightLabel.innerText = `Height: ${height}`
+}
+
+function changeCompressorWidth(evt) {
+    width = evt.target.value
+    compressorWidthLabel.innerText = `Width: ${width}`
+}
+
 compressorBtn.addEventListener('click', compressor)
+compressorHeightSlider.addEventListener('change', changeCompressorHeight)
+compressorWidthSlider.addEventListener('change', changeCompressorWidth)
