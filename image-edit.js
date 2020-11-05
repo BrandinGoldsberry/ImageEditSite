1cea1579b09ff05342e6cdcb48248d0069ee8f3a
shutdoor <enyssa2129@gmail.com>
2020-11-05 10:50:12 -0700

    Add files via upload

--- image-edit.js
+++ image-edit.js
@@ -1,11 +1,92 @@
-window.addEventListener("load", function(){
-    Grade(document.querySelectorAll("#gradientWrap"));
+var imageEdit = {
+    currentImage: {}
+}
 
-    var img = document.querySelector("#image");
-    img.naturalWidth
+const compressorHeightSlider = document.getElementById("compressorHeight");
+const compressorWidthSlider = document.getElementById("compressorWidth");
+const compressorHeightLabel = document.getElementById("compressorHeightLabel");
+const compressorWidthLabel = document.getElementById("compressorWidthLabel");
 
-    // document.getElementById("gradientWrap").style.width = `${img.naturalWidth+40}px`;
-    // console.log(img.naturalWidth);
-    document.getElementById("gradientWrap").style.height = `${img.naturalHeight+60}px`;
-    console.log(img.naturalHeight);
+var height = 50;
+var width = 50;
+
+const compressorBtn = document.getElementById("compressorBtn");
+const gradientWrapDiv = document.getElementById("gradientWrap");
+
+
+const setVals = (fr) => {
+    //Display image for nice styling, allows for scaling without editing the real image
+    document.getElementById("preview").src = fr.result;
+    //Hidden real image that the editing and data comes from
+    document.getElementById("edit-image").src = fr.result;
+    document.getElementById("edit-image").onload = () => {
+        compressorHeightSlider.max = document.getElementById("edit-image").height;
+        compressorHeightSlider.value = document.getElementById("edit-image").height;
+        compressorHeightLabel.innerText = `Height: ${document.getElementById("edit-image").height}`;
+        compressorWidthSlider.max = document.getElementById("edit-image").width;
+        compressorWidthSlider.value = document.getElementById("edit-image").width;
+        compressorWidthLabel.innerText = `Width: ${document.getElementById("edit-image").width}`;
+        height = compressorWidthSlider.value = document.getElementById("edit-image").height;
+        width = compressorWidthSlider.value = document.getElementById("edit-image").width;
+    }
+}
+
+window.onload = (ev) => {
+    document.getElementById("upload-image").addEventListener("change", (imageEvent) => {
+        gradientWrapDiv.style.backgroundImage = "";
+        imageEdit.currentImage = imageEvent.target.files[0];
+        if (FileReader) {
+            var fr = new FileReader();
+            fr.onload = function () {
+               setVals(fr); 
+            }
+            fr.readAsDataURL(imageEvent.target.files[0]);
+        }
+    })
+};
+
+function compressor() {
+    new Compressor(imageEdit.currentImage, {
+        maxHeight: height,
+        maxWidth: width,
+        success(result) {
+            imageEdit.currentImage = result
+            document.getElementById("preview").src = URL.createObjectURL(result);
+            document.getElementById("edit-image").src = URL.createObjectURL(result);
+            compressorHeightSlider.max = document.getElementById("edit-image").height;
+            compressorHeightSlider.value = document.getElementById("edit-image").height;
+            compressorHeightLabel.innerText = `Height: ${document.getElementById("edit-image").height}`;
+            compressorWidthSlider.max = document.getElementById("edit-image").width;
+            compressorWidthSlider.value = document.getElementById("edit-image").width;
+            compressorWidthLabel.innerText = `Width: ${document.getElementById("edit-image").width}`;
+            height = compressorWidthSlider.value = document.getElementById("edit-image").height;
+            width = compressorWidthSlider.value = document.getElementById("edit-image").width;
+        }
+    })
+}
+
+const buttonGrade = document.getElementById("autoGradient");
+const previewImage = document.getElementById("preview");
+var gradeJSBool = false;
+
+buttonGrade.addEventListener("click", function(){
+    if(gradeJSBool === false){
+        Grade(document.querySelectorAll("#gradientWrap"));
+        previewImage.style.boxShadow = "none";
+        gradientWrapDiv.style.boxShadow = "box-shadow: 0px 0px 12px 0px rgba(0,0,0,0.25)";
+        gradeJSBool = true;
+    }else{
+        gradientWrapDiv.style.backgroundImage = "none";
+        gradeJSBool = false;
+    }
+});
+
+compressorBtn.addEventListener('click', compressor)
+compressorHeightSlider.addEventListener('change', (e) => {
+    height = e.target.value
+    compressorHeightLabel.innerText = `Height: ${height}`
+})
+compressorWidthSlider.addEventListener('change', (e) => {
+    width = e.target.value
+    compressorWidthLabel.innerText = `Width: ${width}`
 })
